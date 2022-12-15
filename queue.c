e41ac3989c062d6eb07a8422a2234a5b21feea93
Sanbruce1A <saniimoleayo@gmail.com>
2022-12-15 06:14:05 -0800

    Task updated

--- queue.c
+++ queue.c
@@ -1,10 +1,12 @@
 #include "monty.h"
+
 /**
  * f_queue - prints the top
  * @head: stack head
  * @counter: line_number
  * Return: no return
 */
+
 void f_queue(stack_t **head, unsigned int counter)
 {
 	(void)head;
